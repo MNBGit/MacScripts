e7b079f38e76c934897c0a776c1af13717fd6c6d
Mal <54320821+MNBGit@users.noreply.github.com>
2024-04-11 10:48:12 +0200

    Create Lowercase.sh

---
+++ Lowercase.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script that change all the filename of a folder where it is executed to 
+# First letter in Uppercase and the rest in lowercase
+# Usage / Ex :
+# "ARTIST - TITLE" to "Artist - Title"
+
+for file in *; do
+       newname=$(echo "$file" | LC_ALL=C.UTF-8 awk '{for (i=1; i<=NF; i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2))}1')
+       mv "$file" "$newname"
+done
